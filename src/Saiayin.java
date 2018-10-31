0b71f2a9214c2cb36046f9736e506f644501334c
mateo <mateo@192.168.0.108>
2018-10-30 21:48:17 -0300

    encontar error

---
+++ src/Saiayin.java
@@ -0,0 +1,16 @@
+
+class Saiayin {
+    String nombre;
+    String planeta;
+    int nivel;
+    int ki;
+
+    public  Saiayin(String nombre, String planeta, int nivel, int ki) {
+        this.nombre = nombre;
+        this.planeta = planeta;
+        this.nivel = nivel;
+        this.ki = ki;
+        System.out.println("***Datos del guerrero Z***");
+    }
+    
+}
---
+++ src/publicaPrincipal.java
@@ -0,0 +1,19 @@
+
+public class publicaPrincipal {
+   
+    
+    public static void main( String[] args) {
+        
+        Saiayin s1 = new Saiayin("goku","vegito",4,152);
+        
+        System.out.println(s1.nombre);
+        System.out.println(s1.planeta);
+        System.out.println(s1.nivel);
+        System.out.println(s1.ki);
+        
+       
+                }
+    
+
+    
+}
